cd786463fc31fab59c3aa62ef3b139d3b4489f4e
Mohammad Zafar <mzcoder7@gmail.com>
2021-10-12 16:30:22 +0100

    Build schema

--- models/workout.js
+++ models/workout.js
@@ -1,2 +1,51 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now,
+  },
+
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: "Enter type of exercise",
+      },
+
+      name: {
+        type: String,
+        trim: true,
+        required: "Enter name of exercise",
+      },
+
+      duration: {
+        type: Number,
+        trim: true,
+        required: "Enter duration in minutes",
+      },
+
+      weight: {
+        type: Number,
+        trim: true,
+      },
+
+      reps: {
+        type: Number,
+        trim: true,
+      },
+
+      sets: {
+        type: Number,
+        trim: true,
+      },
+
+      distance: {
+        type: Number,
+        trim: true,
+      },
+    },
+  ],
+});
