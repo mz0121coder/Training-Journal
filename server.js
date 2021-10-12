f4e2aa8b008faae750426857cfd5de2945374d2b
Mohammad Zafar <mzcoder7@gmail.com>
2021-10-12 16:32:30 +0100

    Connect URI (mongodb) to mongoose

--- server.js
+++ server.js
@@ -9,3 +9,10 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/wt-1", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  userFindAndModify: false,
+});
