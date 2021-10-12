83511ad8585406cae110a60a4d77fb444e6c88b8
Mohammad Zafar <mzcoder7@gmail.com>
2021-10-12 16:36:16 +0100

    Update server.js config

--- server.js
+++ server.js
@@ -34,6 +34,18 @@ app.get("/api/workouts", async (req, res) => {
   res.json(workouts);
 });
 
+// Organise stats in descending order (last in, first out)
+app.get("/api/workouts/range", async (req, res) => {
+  const workouts = await db.Workout.aggregate([
+    { $addFields: { totalDuration: { $sum: "$exercises.duration" } } },
+  ])
+    .sort({
+      _id: -1,
+    })
+    .limit(7);
+  res.json(workouts);
+});
+
 // Routes to add & update exercises
 app.post("/api/workouts", async (req, res) => {
   db.Workout.create(req.body).then((newWorkout) => res.json(newWorkout));
